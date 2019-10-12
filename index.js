8b3ed3b18752dc71c0d280a22ff77b6bc826d27a
Erdemio <erdem_arslan_694@hotmail.com>
2019-10-12 18:52:26 +0300

    Deploy#4, Now command will chose from database.

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const request = require('request');
 
 const configs = [
   process.env.BOT_TOKEN
 ];
 
 
-client.on('ready', () => {
-  console.log('BOT# Mounted and loaded.');
+client.on('ready', function() {
+  console.log("And Loaded " + client.guilds.size + " servers");
+  var times = 1;
+  const activities_list = [
+    "help",
+    client.guilds.size + " servers",
+  ];
+
+  setInterval(() => {
+    var index = times % 2;
+    times++;
+    client.user.setActivity(activities_list[index], {
+      type: "WATCHING"
+    });
+  }, 10000);
 });
 
 
 client.on('message', message => {
+  if (message.author.equals(client.user))
+    return;
 
-  if (message.content === 'hi') {
-    message.channel.send('hello');
-  }
-});
+  request('https://api.erdem.in/ebot/processor.php?cmd='+message.content, function (error, response, body) {
+    if (response.statusCode===200) {
+      var resResponse = JSON.parse(body);
+      var chatResponse = resResponse['response'];
+
+        if (chatResponse!="-1") {
+          message.channel.send(chatResponse);
+        }
+
+
+    }else{ console.log("BOT# Site shut down."); }
+  }); //End of request.
+
+
+
+}); // End  of message.
 
 client.login(configs[0]);
