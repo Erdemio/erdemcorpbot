7e52588ed0d1877e7e06a27187e5a3f0ec52b8a9
Erdemio <erdem_arslan_694@hotmail.com>
2019-10-12 19:26:16 +0300

    Deploy#4.3

--- index.js
+++ index.js
@@ -27,20 +27,46 @@ client.on('message', message => {
   if (message.author.equals(client.user))
     return;
 
-    console.log(message.content);
-  request('https://api.erdem.in/ebot/processor.php?cmd='+message.content, function (error, response, body) {
-    if (response.statusCode===200) {
-      var resResponse = JSON.parse(body);
-      var chatResponse = resResponse['response'];
+    if (message.channel.type == 'dm') {
+      var prefix = "";
+      var m = message.content.substring(prefix.length).split(" ");
 
-        if (chatResponse!="-1") {
-          message.channel.send(chatResponse);
-          chatResponse = "";
-        }
+      switch (m[0].toLowerCase()) {
+        case "insert":
+              request('https://api.erdem.in/ebot/processor.php?proccess=insert&command='+m[1]+'&response='+m[2], function (error, response, body) {
+                if (response.statusCode===200) {
+                  var resResponse = JSON.parse(body);
+                  var chatResponse = resResponse['status'];
 
+                    message.author.send(chatResponse);
 
-    }else{ console.log("BOT# Site shut down."); }
-  }); //End of request.
+                }else{ console.log("BOT# Site shut down."); }
+
+              }); //End of request.
+        break; // End of 'case:insert'.
+        default:
+          message.author.send("It's easy to find out! You can do it with your brain!");
+          break; // End of 'case:default'
+      }
+    } // End of dm.
+    else
+    {
+        //Normal chat.
+      console.log(message.content);
+      request('https://api.erdem.in/ebot/processor.php?cmd='+message.content, function (error, response, body) {
+        if (response.statusCode===200) {
+          var resResponse = JSON.parse(body);
+          var chatResponse = resResponse['response'];
+
+            if (chatResponse!="-1") {
+              message.channel.send(chatResponse);
+              chatResponse = "";
+            }
+        }else{ console.log("BOT# Site shut down."); }
+
+      }); //End of request.
+
+    } // End of normal chat.
 
 }); // End  of message.
 
